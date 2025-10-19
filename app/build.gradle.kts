ea7a65616737ea9d07f08b9607ed5e058aac2c01
FreetimeMaker <freetimemaker123321@gmail.com>
2025-10-19 18:37:48 +0200

    update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.freetime.radio"
-        minSdk = 26
+        minSdk = 24
         targetSdk = 36
         versionCode = 1
         versionName = "1.0.0"
